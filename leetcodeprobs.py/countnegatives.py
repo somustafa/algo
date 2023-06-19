80dd74669aacdc67ee36319a5fc7122ab6ee97e6
somustafa <so.mystafa@gmail.com>
2023-06-19 18:48:47 +0400

    conver rad to degree

---
+++ leetcodeprobs.py/countnegatives.py
@@ -0,0 +1,7 @@
+def countNegatives(self, grid: List[List[int]]) -> int:
+    n=0
+    m=[0]
+    while list < 0:
+        m+=1
+        n=n+1
+        
---
+++ rad_to_degree.py
@@ -0,0 +1,8 @@
+rad=int(input(Enter your radian: ))
+degree=rad * 57.2957795 
+print(rad)
+
+
+
+
+#1 rad= 57.2957795 degree

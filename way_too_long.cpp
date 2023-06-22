bf80163c33ef219cde297e5180cbd72b9376621a
somustafa <so.mystafa@gmail.com>
2023-06-23 01:05:58 +0400

    fix formatting

--- way_too_long.cpp
+++ way_too_long.cpp
@@ -5,7 +5,7 @@ int main(){
     int n;
     std::cin >> n;
     std::string s;
-    for (int i = 0; i < n; ++i){
+    for (int i = 0; i < n; ++i) {
         std::cin >> s;
         if (s.size() > 10){
             std::cout << s[0] << s.size() - 2 << s[s.size() - 1] << "\n";
